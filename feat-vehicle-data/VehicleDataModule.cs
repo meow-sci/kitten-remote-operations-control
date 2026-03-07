9efb654731ded1d6e3f529285057635a37030d5c
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-07 08:32:17 -0500

    feat(vehicle-data): implement GET /vehicle/data/list endpoint

--- feat-vehicle-data/VehicleDataModule.cs
+++ feat-vehicle-data/VehicleDataModule.cs
@@ -1,25 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GenHTTP.Api.Content;
 using GenHTTP.Api.Protocol;
 using GenHTTP.Modules.Functional;
-using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Layouting;
 using GenHTTP.Modules.Layouting.Provider;
+using KSA;
 using KROC.Server;
 
 namespace KROC.FeatVehicleData;
 
-/// <summary>
-/// VehicleData feature module
-/// </summary>
 public sealed class VehicleDataModule : IEndpointModule
 {
-  public void Register(LayoutBuilder routes)
-  {
-    var pong = Inline.Create()
-                     .Get((IRequest request) =>
-                         request.Respond()
-                                .Content("pong")
-                                .Type(FlexibleContentType.Get(ContentType.TextPlain))
-                                .Build());
+    public void Register(LayoutBuilder routes)
+    {
+        var dataLayout = Layout.Create()
+            .Add("list", Inline.Create().Get(() =>
+            {
+                try
+                {
+                    var vehicles = Universe.CurrentSystem?.Vehicles.GetList() ?? Enumerable.Empty<Vehicle>();
+                    Vehicle? ctrl = Program.ControlledVehicle;
 
-    routes.Add("pongz", pong);
-  }
+                    var items = vehicles
+                        .Select(v => new VehicleListItem(v.Id, v.Id, ctrl is not null && v.Id == ctrl.Id))
+                        .ToList();
+
+                    return (object)new ApiResponse<List<VehicleListItem>>("ok", items);
+                }
+                catch (Exception ex)
+                {
+                    throw new ProviderException(ResponseStatus.InternalServerError,
+                        "Unexpected error while listing vehicles.", ex);
+                }
+            }));
+
+        routes.Add("vehicle", Layout.Create()
+            .Add("data", dataLayout));
+    }
 }
+
+public record VehicleListItem(string Id, string Name, bool IsControlled);
+public record ApiResponse<T>(string Status, T? Data);
